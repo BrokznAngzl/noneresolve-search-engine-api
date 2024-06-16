f18191c407019681435643e1c753023b9fa9e192
Wisarut Beng <bukobeer001@gmail.com>
2024-06-17 05:30:50 +0700

    upadte get detail algori

--- tfidf_service.py
+++ tfidf_service.py
@@ -1,10 +1,7 @@
-import requests
-from bs4 import BeautifulSoup
-
 from pythainlp import word_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from urllib.parse import urljoin
+
 from nomalizing import normalize
 
 
@@ -26,35 +23,6 @@ class TfidfService:
         return TfidfService.TDIDFVECTORIZER.transform([query])
 
     @staticmethod
-    def getPageInfo(url):
-        try:
-            response = requests.get(url)
-            soup = BeautifulSoup(response.content, 'html.parser')
-
-            title = soup.title.string.strip() if soup.title else None
-            if title:
-                title_words = title.split()[:2]
-                title = ' '.join(title_words)
-
-            icon_url = None
-            for tag in soup.find_all('link', rel=lambda value: value and value.lower() in ['icon', 'shortcut icon']):
-                icon_url = urljoin(url, tag['href'])
-                break
-
-            description = None
-            for tag in soup.find_all('p'):
-                text = tag.get_text().strip()
-                if len(text) > 0:
-                    description = text[:200]
-                    break
-
-            return title, icon_url, description
-
-        except Exception as e:
-            print(f"Error fetching information from {url}: {e}")
-            return None, None, None
-
-    @staticmethod
     def search(query, result_quantity, docs):
         clear_words = []
         query_word_seg = word_tokenize(query, keep_whitespace=False)
@@ -73,7 +41,11 @@ class TfidfService:
             link = docs['doc-link'][doc_idx]
             title = docs['doc-title'][doc_idx].split()[0]
             icon = docs['doc-icon'][doc_idx]
-            body = docs['doc-body'][doc_idx]
+
+            # the problem is in my inserting web data in my database
+            body = docs['doc-body'][doc_idx].replace(
+                'เข้าสู่ระบบ MIS บุคลากรคณะ ICSEC2022 การเข้าศึกษา การรับสมัครและคุณสมบัติผู้สมัคร ปฏิทินการศึกษา ค่าธรรมเนียมการศึกษา หลักสูตร ภาควิชา ภาควิชาวิศวกรรมเครื่องกลและการผลิต ภาควิชาวิศวกรรมไฟฟ้าและคอมพิวเตอร์ ภาควิชาวิศวกรรมโยธาและสิ่งแวดล้อม ภาควิชาวิทยาการคอมพิวเตอร์และสารสนเทศ ภาควิชาวิทยาศาสตร์ทั่วไป การรับสมัครและคุณสมบัติผู้สมัคร ปฏิทินการศึกษา ค่าธรรมเนียมการศึกษา 13 หลักสูตร หลักสูตรสาขาวิชาวิศวกรรมโยธา หลักสูตรสาขาวิชาวิศวกรรมไฟฟ้า หลักสูตรสาขาวิชาวิศวกรรมเครื่องกลและการผลิต หลักสูตรสาขาวิชาวิศวกรรมอุตสาหการ หลักสูตรสาขาวิชาวิศวกรรมคอมพิวเตอร์ หลักสูตรสาขาวิชาวิศวกรรมสิ่งแวดล้อมเพื่อการพัฒนาอย่างยั่งยืน หลักสูตรสาขาวิชาวิทยาการคอมพิวเตอร์ หลักสูตรสาขาวิชาเคมีประยุกต์ หลักสูตรสาขาวิชาวิทยาการข้อมูล หลักสูตรสาขาวิชาเทคโนโลยีพลังงานเพื่อความยั่งยืน หลักสูตรสาขาวิชาการจัดการวิศวกรรมและเทคโนโลยี (วศ.ม.) หลักสูตรสาขาวิชาวิศวกรรมไฟฟ้าและคอมพิวเตอร์ (วศ.ม.) หลักสูตรสาขาวิชาวิศวกรรมไฟฟ้าและคอมพิวเตอร์ (ปร.ด.) ภาควิชาวิศวกรรมเครื่องกลและการผลิต ภาควิชาวิศวกรรมไฟฟ้าและคอมพิวเตอร์ ภาควิชาวิศวกรรมโยธาและสิ่งแวดล้อม ภาควิชาวิทยาการคอมพิวเตอร์และสารสนเทศ ภาควิชาวิทยาศาสตร์ทั่วไป ระดับปริญญาตรี ระดับบัณฑิตศึกษา ภาควิชาวิศวกรรมเครื่องกลและการผลิต ภาควิชาวิศวกรรมไฟฟ้าและคอมพิวเตอร์ ภาควิชาวิศวกรรมโยธาและสิ่งแวดล้อม ภาควิชาวิทยาการคอมพิวเตอร์และสารสนเทศ ภาควิชาวิทยาศาสตร์ทั่วไป บุคลากรสำนักงานเลขานุการคณะ สำหรับบุคลากร สำหรับนิสิต ประกาศคณะ ประชาสัมพันธ์ รับเข้าศึกษา วิจัยและบริการวิชาการ วิเทศสัมพันธ์ เทคโนโลยีดิจิทัล ข่าวจ้างเหมา/ประกวดราคา แสดงความยินดี จดหมายเวียน โครงสร้างการบริหาร คณะกรรมการประจำคณะ ประวัติคณะ วิสัยทัศน์ พันธกิจ ทำเนียบคณบดี แผนยุทธศาสตร์ 2563–2567 ระดับปริญญาตรี หลักสูตรที่เปิดสอน ช่องทางการรับเข้าศึกษา คุณสมบัติของผู้สมัคร ค่าธรรมเนียมการศึกษา สมัครเข้าศึกษาต่อ ทุนการศึกษา หอพัก ลิงก์ที่เกี่ยวข้อง สวพ.มก. วช. สวก. อว. NRIIS (วช.) วารสาร การประชุมวิชาการ นำไปใช้ประโยชน์ กลุ่มวิจัยและนวัตกรรมอิเล็กทรอนิกส์ชีวภาพ SMART (Systematic Manufacturing Alternative Research Team) กลุ่มวิจัยพลังงานและสิ่งแวดล้อม กลุ่มวิจัยสมุนไพร กลุ่มวิจัยนวัตกรรมวัสดุ (Innovation Material Research Group) หน่วยบริการเครื่องมือวิทยาศาสตร์ ศูนย์วิจัยสมุนไพร ศูนย์ทดสอบและวิจัยทางด้านวิศวกรรมโยธา งานวิเคราะห์น้ำ ค่ายวิทยาศาสตร์ ผลิตภัณฑ์กัญชา U2T เมนู ออกจากระบบ เกี่ยวกับคณะ ข้อมูลการรับเข้า ข้อมูลภาควิชา ระบบสารสนเทศ ข่าวสาร/ประกาศ ติดต่อ'
+                , ' ')
 
             print(f"{i + 1}. {link} (Similarity score: {similarity_score:.4f})")
             json_data = {
